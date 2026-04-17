3b0d9fd4ef7c13b67a3e633074d32880b7d4d910
467480903 <your_email@example.com>
2026-04-17 18:33:14 +0800

    SDFASDFADF

--- p415/move.py
+++ p415/move.py
@@ -4,7 +4,7 @@ import math
 robot = RobotController()
 
 # 导航到地图第0个点
-robot.go(8)
+robot.go(0)
 
 # 高精度导航
-robot.go(8, high_precision=True)
+# robot.go(8, high_precision=True)
--- p415/move_to_place.py
+++ p415/move_to_place.py
@@ -1,9 +1,16 @@
 from robot_controller import RobotController
 import math
+import time
 
 robot = RobotController()
 
 # 导航到地图第0个点
+# robot.go(9)
+# time.sleep(0.5)
+# robot.go(10)
+# time.sleep(0.5)
+# robot.go(4)
+# time.sleep(0.5)
 robot.go(5)
 
 # 高精度导航
